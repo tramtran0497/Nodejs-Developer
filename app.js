5637a3ad39a22009447d5c5dd7e3e8ae5f4d0c8d
tramtran0497 <tramtran0497@gmail.com>
2022-04-12 13:23:47 +0300

    USing postman-request npm to call request API from url

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 // const { default: chalk } = require("chalk");
 // const msg = chalk.red.bold("Tram")
 
+require('dotenv').config()
+const request = require('postman-request');
+
+const access_key = process.env.ACCESS_KEY
+
+const url = `http://api.weatherstack.com/current?access_key=${access_key}&query=Helsinki`
+
+request(url, (error, response, body) => {
+    // parse data JSON string to JS Obj
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+});
