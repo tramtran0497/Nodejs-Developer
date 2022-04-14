6e2f59afb5d9af2689ca264ca5e3e15527a99f41
tramtran0497 <tramtran0497@gmail.com>
2022-04-14 11:32:30 +0300

    Objext destructuring

--- app.js
+++ app.js
@@ -10,15 +10,15 @@ const place = process.argv[2];
 if(!place){
     console.log(chalk.red.inverse("Please fill the city that you are looking for weather!"));
 } else{
-    geocode(place, (error, data) => {
+    geocode(place, (error, {placeName}) => {
         if(error){
             return console.log(error);
         }
-        forecast(data.placeName, (error, forecastData) => {
+        forecast(placeName, (error, forecastData) => {
             if(error) {
                 return console.log(error);
             }
-            console.log("Place: ", data.placeName);
+            console.log("Place: ", placeName);
             console.log("Weather: ", forecastData);
         });
     });
