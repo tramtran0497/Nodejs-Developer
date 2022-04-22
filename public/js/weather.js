ab20a486b1104bf0f62c4ab953e43e2a03eea77f
tramtran0497 <tramtran0497@gmail.com>
2022-04-22 13:05:25 +0300

    Showing place and weather after searching by UI

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,16 +1,27 @@
 
 const weatherForm = document.querySelector("form");
 const searchInput = document.querySelector("input");
+const message1 = document.querySelector(".message1");
+const message2 = document.querySelector(".message2");
+
 
 weatherForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const location = searchInput.value;
+    message1.textContent = "LOADING...";
+    message2.textContent = "";
     fetchData(location);
 });
 
 const fetchData = async(location) => {
     const resAPI = await fetch(`http://localhost:5000/weatherAPI?place=${location}`);
     const resData = await resAPI.json();
-    if(resData.error) return console.log(resData.error);
-    return console.log(resData);
+    if(resData.error){
+        message1.textContent = resData.error;
+    } else{
+       return[
+            message1.textContent = resData.place,
+            message2.textContent = resData.weather
+       ]
+    };
 };
