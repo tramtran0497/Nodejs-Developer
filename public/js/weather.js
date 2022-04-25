50c8f80d3da35dc2ed3d34f55e950c4678d5e077
tramtran0497 <tramtran0497@gmail.com>
2022-04-25 14:41:13 +0300

    Set up several things before deploying Heroku

--- public/js/weather.js
+++ public/js/weather.js
@@ -14,7 +14,7 @@ weatherForm.addEventListener("submit", (event) => {
 });
 
 const fetchData = async(location) => {
-    const resAPI = await fetch(`http://localhost:5000/weatherAPI?place=${location}`);
+    const resAPI = await fetch(`/weatherAPI?place=${location}`);
     const resData = await resAPI.json();
     if(resData.error){
         message1.textContent = resData.error;
